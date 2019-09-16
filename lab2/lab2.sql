ce752331ef7ac434476c1422166ae81e2b123ec8
Axel <am_johansson2@hotmail.com>
2019-09-16 14:03:46 +0200

    vörks

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,3 +1,6 @@
-CREATE MATERIALIZED VIEW PopData AS
-  SELECT year, name, population, country, longitude
-  FROM citypops
+--CREATE MATERIALIZED VIEW PopData AS
+  SELECT citypops.country, citypops.year, city.name, citypops.population,
+         city.longitude, city.latitude, city.elevation,
+         economy.agriculture, economy.service, economy.industry, economy.inflation
+  FROM citypops, city, economy
+  WHERE city.name = citypops.city;
