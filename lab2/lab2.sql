e9123035b7d642a6b1bd421b3c1c9bfec8e36f4d
Axel <am_johansson2@hotmail.com>
2019-09-16 14:29:36 +0200

    1a pretty much works

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,6 +1,9 @@
---CREATE MATERIALIZED VIEW PopData AS
+DROP MATERIALIZED VIEW PopData;
+CREATE MATERIALIZED VIEW PopData AS
   SELECT citypops.country, citypops.year, city.name, citypops.population,
          city.longitude, city.latitude, city.elevation,
          economy.agriculture, economy.service, economy.industry, economy.inflation
   FROM citypops, city, economy
-  WHERE city.name = citypops.city;
+  WHERE city.name = citypops.city AND economy.country = city.country;
+
+SELECT * FROM PopData WHERE name LIKE 'Beograd';
