1913a4f21833cdc15e3e3d31c7f98d3bb7fc9c2b
Axel <am_johansson2@hotmail.com>
2019-09-16 14:07:26 +0200

    DROP mat. view

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,4 +1,5 @@
---CREATE MATERIALIZED VIEW PopData AS
+DROP MATERIALIZED VIEW PopData;
+CREATE MATERIALIZED VIEW PopData AS
   SELECT citypops.country, citypops.year, city.name, citypops.population,
          city.longitude, city.latitude, city.elevation,
          economy.agriculture, economy.service, economy.industry, economy.inflation
