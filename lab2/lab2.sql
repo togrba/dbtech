481423e301033b395c57fa63007516071f0574ec
Tove <togrba@gmail.com>
2019-09-16 14:07:33 +0200

    last

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,6 +1,8 @@
---CREATE MATERIALIZED VIEW PopData AS
+CREATE MATERIALIZED VIEW PopData AS
   SELECT citypops.country, citypops.year, city.name, citypops.population,
          city.longitude, city.latitude, city.elevation,
          economy.agriculture, economy.service, economy.industry, economy.inflation
   FROM citypops, city, economy
   WHERE city.name = citypops.city;
+
+SELECT * FROM popdata WHERE city LIKE 'Santiago%';
