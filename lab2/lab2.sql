34345543a6e90d45e270faea2354a00730eb9468
Tove <togrba@gmail.com>
2019-09-17 17:16:39 +0200

    duplicates in table

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,16 +1,26 @@
 --DROP MATERIALIZED VIEW PopData;
 CREATE MATERIALIZED VIEW PopData AS
-  SELECT city, country, year, population
-  FROM citypops
-  JOIN city ON citypops.city = city.name;
+  -- SELECT
+  -- citypops.year, citypops.city, citypops.population, citypops.country,
+  -- city.longitude, city.latitude, city.elevation,
+  -- economy.agriculture, economy.service, economy.industry, economy.inflation
+  -- FROM citypops
+  -- JOIN city ON citypops.city = city.name
+  -- JOIN economy ON citypops.country = economy.country;
 
-  -- SELECT longitude, latitude, elevation
-  -- FROM city
-  -- UNION ALL
-  -- SELECT agriculture, service, industry, inflation
-  -- FROM economy;
 
-SELECT * FROM PopData WHERE city LIKE 'Santiago%';
+  SELECT
+  year, city, cp.population, cp.country,
+  longitude, latitude, elevation,
+  agriculture, service, industry, inflation
+  FROM citypops cp, city c, economy e
+  WHERE cp.city = c.name AND c.country = e.country;
+
+
+-- city attributes doubles
+
+
+ SELECT * FROM PopData WHERE city LIKE 'Santiago%';
 
 
 -- run: \i lab2.sql
